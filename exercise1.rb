52443b5de775a49bf4502f071c14542cffcc81fb
cordell <cordell.perry@gmail.com>
2017-10-30 22:28:52 -0400

    escape sequences

--- exercise1.rb
+++ exercise1.rb
@@ -1,11 +1,21 @@
-puts 2 + 3
+# puts 2 + 3
+#
+# puts 2
+# puts 3
+# puts 2 + 3
+#
+# puts 2 puts 3 puts 2 + 3
+#
+# puts 2
+# puts 3
+# puts 2 + 3
 
-puts 2
-puts 3
-puts 2 + 3
+# puts "ada lovelace lived for #{1852 - 1815} years."
 
-puts 2 puts 3 puts 2 + 3
+# puts "hello\t\tworld"
 
-puts 2
-puts 3
-puts 2 + 3
+# puts "hello \b\b\b\b\bgoodbye world"
+
+# puts "Hello\rStart over world"
+
+puts "1. Hello\n2. World"
