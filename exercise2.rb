dd12e37f7942e1b05eb27def0a557652335e0bce
cordell <cordell.perry@gmail.com>
2017-10-31 09:49:03 -0400

    gets exercise

--- exercise2.rb
+++ exercise2.rb
@@ -4,4 +4,8 @@
 # discount = (cost * 0.18)
 # puts discount
 
-puts "string of words #{55}"
+# puts "string of words #{55}"
+
+# puts "total #{45628 * 7839}"
+
+(10 < 20 && 30 < 20) || !(10 == 11)
---
+++ exercise3.rb
@@ -0,0 +1,5 @@
+puts "What is your name?"
+
+puts "What is your name?"
+user_name = gets
+puts "Hello, #{user_name}"
