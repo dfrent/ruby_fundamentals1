232b4e95e4910307d20c6aecae0a3a43750c3b3c
Your Name <you@example.com>
2017-11-01 15:22:55 -0400

    exersice 4-4

---
+++ exersice4-4.rb
@@ -0,0 +1,16 @@
+user_name = gets.chomp
+
+
+if user_name.length < 10
+	puts "hi, #{user_name}"
+elsif user_name.length >  10
+	puts "hello,#{user_name} "
+else user_name.length  == 10
+	puts "hey, #{user_name}"
+end	
+
+
+
+# secret_number = 17
+
+#  # "enter secret number"
